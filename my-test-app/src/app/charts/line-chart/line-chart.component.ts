74af1757aaab21d9a01db13684df6b1322cdcf51
MAG <marcin.gala@ticos-systems.ch>
2019-10-14 13:13:05 +0200

    add a charts

--- my-test-app/src/app/charts/line-chart/line-chart.component.ts
+++ my-test-app/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [32, 14, 46, 13, 55], label: 'Sentiment Analysis'},
+  {data: [12, 15, 44, 12, 33], label: 'Image Recognition'},
+  {data: [56, 23, 46, 7, 18], label: 'Forecasting'},
+];
+
+const LINE_CHART_LABELS: string [] = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +18,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
--- my-test-app/src/app/charts/pie-chart/pie-chart.component.ts
+++ my-test-app/src/app/charts/pie-chart/pie-chart.component.ts
@@ -5,9 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
+
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+
+  pieChartData: number[] = [350, 450, 120];
+  colors: any [] = [
+  {
+    backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+    borderColor: '#999'
+  }
+];
+  pieChartLabels: string[] = ['XYZ Logistics', 'Main St Bakery', 'ASP NET CORE'];
+  // pieChartType = 'pie';
+  pieChartType = 'doughnut';
+
 
   ngOnInit() {
   }
