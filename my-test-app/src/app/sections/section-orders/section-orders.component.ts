ef42aa3c774dbf59c3dc90f946cf7e3f7efc9b4f
MAG <marcin.gala@ticos-systems.ch>
2019-10-14 14:50:34 +0200

    add latest orders view

--- my-test-app/src/app/sections/section-orders/section-orders.component.ts
+++ my-test-app/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
+
 
 @Component({
   selector: 'app-section-orders',
@@ -9,7 +11,32 @@ export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
 
+  orders: Order[] = [
+    {id: 1, customer:
+       {id: 1, name: 'Main St Bakery', state: 'CO', email: 'zxc@wp.pl' },
+       total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 2, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'zxc@wp.pl' },
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 3, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'zxc@wp.pl' },
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 4, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'zxc@wp.pl' },
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 5, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'zxc@wp.pl' },
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)}
+  ];
+
   ngOnInit() {
   }
 
 }
+
+// order.id
+// order.customer.name
+// order.total
+// order.placed
+// order.fulfilled
+// Status
